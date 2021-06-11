acb993942fa0e2f73b4f352589df757a754b5f7c
thaysom22 <thaysom22@gmail.com>
2021-06-11 04:25:48 +0000

    feat: create subscription model in subscribe app

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class Subscription(models.Model):
+    """
+    Stores data about a user subscription
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(blank=False, null=False, max_length=254)
+    first_name = models.CharField(blank=False, null=False, max_length=50)
+    last_name = models.CharField(blank=False, null=False, max_length=50)
+    start_date = models.DateField(auto_now_add=True)
+    country = CountryField(blank_label='Country', blank=True, null=True)  # relates country names to ISO codes
+    city = models.CharField(blank=True, null=True, max_length=50)
