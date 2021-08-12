30fbf5274fac3b03b8d88702563f5abe647d9676
thaysom22 <thaysom22@gmail.com>
2021-08-12 22:52:48 +0000

    feat: update navbar styles and layout
    * add color variables and import fonts to stylesheet

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,11 +1,12 @@
 /* 
-attach click event listener to toggle .hide class
-for all content after navbar in document and
-toggle between bars and times icons for 
-collapsible navbar toggler
+attach click event listener navbar toggler to:
+* toggle .hide class for all content after navbar in document
+* toggle .hide class on instagram nav link so it does not display when nav is collapsed
+* toggle between bars and times icons for collapsible navbar toggler
 */
 $('.navbar-toggler').click(() => {
     $('#hide-when-nav-expanded').toggleClass('hide');
+    $('.navbar-nav .nav-instagram').toggleClass('hide');
     $('.navbar-toggler-icon i').toggleClass(['fa-bars', 'fa-times']);
 });
 
