ed975774b16aa1e7a60650fc20fba5745682772c
thaysom22 <thaysom22@gmail.com>
2021-08-13 21:19:56 +0000

    feat: update stories pages
    * add flex container for story image links

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,12 +1,10 @@
 /* 
 attach click event listener navbar toggler to:
 * toggle .hide class for all content after navbar in document
-* toggle .hide class on instagram nav link so it does not display when nav is collapsed
 * toggle between bars and times icons for collapsible navbar toggler
 */
 $('.navbar-toggler').click(() => {
     $('#hide-when-nav-expanded').toggleClass('hide');
-    $('.navbar-nav .nav-instagram').toggleClass('hide');
     $('.navbar-toggler-icon i').toggleClass(['fa-ellipsis-h', 'fa-times']);
 });
 
