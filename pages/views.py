69d5f2bc765104f8826de3dcb7043c9831f87d46
thaysom22 <thaysom22@gmail.com>
2021-08-16 22:37:35 +0000

    feat: add animation to homepage hero image  and interactive effects to buttons

--- pages/views.py
+++ pages/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
-from django.contrib import messages
+
 
 def index(request):
     """ Display homepage """
     template = "pages/index.html"
-
-    # TEST
-    messages.debug(request, 'SQL statements were executed.')
-    messages.info(request, 'Three credits remain in your account.')
-    messages.success(request, 'Profile details updated.')
-    messages.warning(request, 'Your account expires in three days.')
-    messages.error(request, 'Document deleted.')
-
     return render(request, template)
-
 
 def about(request):
     """ Display about page """
