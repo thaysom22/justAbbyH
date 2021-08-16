72a88c8e2ea9bb77fe810fe9158b8e1d7cf96f09
thaysom22 <thaysom22@gmail.com>
2021-08-16 20:40:23 +0000

    feat: add delete story link to edit story page

--- stories/static/stories/js/story_form.js
+++ stories/static/stories/js/story_form.js
@@ -7,3 +7,10 @@ $('.add-story-form #id_genre').on("change", function(event) {
     $(this).css("color", "#061234");
     $(this).off(event);  // handler only runs once
 });
+
+$('#delete-story-link').on("click", function(event) {
+    var response = window.confirm("Are you sure you want to delete this story?");
+    if (!response) {
+        event.preventDefault();
+    }
+});
